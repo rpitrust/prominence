f3dafe61b8926d4eba6a26fc136bc96e557f31ea
lxh529 <lxh529@gmail.com>
2013-06-27 12:03:15 -0400

    update copyright info.

--- CommDistHashmap/main.cpp
+++ CommDistHashmap/main.cpp
@@ -11,25 +11,35 @@
  * 5. in case that we can not sample a node from a community (small community), increase
  * sample probability by k%.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Copyright (c) 2013, Xiaohui Lu, Sibel Adali and Malik Magdon-Ismail
+ * All rights reserved.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *	• Redistributions of source code must retain the above copyright notice, 
+ *	this list of conditions and the following disclaimer.
+ *	• Redistributions in binary form must reproduce the above copyright
+ *	notice, this list of conditions and the following disclaimer in the
+ *	documentation and/or other materials provided with the distribution.
  *
- * See http://www.gnu.org/copyleft/gpl.txt for more details.
+ *	Any publication resulting from the use of this work must cite the
+ *	following publication :
+ *      S. Adali, X. Lu and M. Magdon-Ismail, "Deconstructing centrality:
+ *		thinking locally and ranking globally in networks", accepted to appear in 
+ *		ASONAM 2013
  *
- * File:   main.cpp
- * Contributors: Dr. Sibel Adali (sibel@cs.rpi.edu), Dr. Malik Magdon-Ismail (magdon@cs.rpi.edu)
- * Code Author: Xiaohui Lu (lux3@cs.rpi.edu)
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
